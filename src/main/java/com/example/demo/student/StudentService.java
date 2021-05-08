37ec44e241bae3c4ed7de0af8617a49f813332d7
Mateusz Dylewski s18737 <s18737@pjwstk.edu.pl>
2021-05-08 12:42:06 +0200

    first commit

---
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,29 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Service
+public class StudentService {
+
+    public List<Student> getStudents(){
+        return List.of(
+                new Student(1L,
+                        "Mateusz",
+                        22,
+                        LocalDate.of(1999, Month.OCTOBER, 6),
+                        "mateuszdylewski@yahoo.com"
+                ),
+                new Student(2L,
+                        "Adam",
+                        32,
+                        LocalDate.of(1988, Month.APRIL, 28),
+                        "adamturek@gmail.com"
+                )
+        );
+    }
+}
